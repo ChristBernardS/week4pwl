e7e00a1d611e27e778d3fb104edf7ceec6f90535
Christopher B. S. <117885718+ChristBernardS@users.noreply.github.com>
2025-09-29 16:07:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+// Part 1
+// if(true)
+// {
+//     pass
+// } else {
+//     pass
+// }
+
+
+// const getDay = () => {
+//     if (new Date().getDay() === 0) {
+//         return "Sunday"
+//     }
+//     if (new Date().getDay() === 1) {
+//         return "Monday"
+//     }
+//     if (new Date().getDay() === 2) {
+//         return "Tuesday"
+//     }
+//     if (new Date().getDay() === 3) {
+//         return "Wednesday"
+//     }
+//     if (new Date().getDay() === 4) {
+//         return "Thursday"
+//     }
+//     if (new Date().getDay() === 5) {
+//         return "Friday"
+//     }
+//     if (new Date().getDay() === 6) {
+//         return "Saturday"
+//     }
+// }
+
+// document.getElementById("demo").innerHTML = `Today is ${getDay(0)}`
+
+// Part 2
+// Data
+const today = {
+    day: 1,
+    name: "Monday"
+}
+const harilain = {
+    day: 5,
+    name: "Friday"
+}
+
+// // Ternary
+// new Date().getDay() === today.day ? (console.log(`today is ${today.name}`)) : (console.log("Not Today"))
+
+// // Ternary single condition
+// new Date().getDay() === today.day
+// ? (console.log(`today is ${today.name}`))
+// : (console.log(`today is not ${today.name}`))
+
+// // Multiple condition
+// new Date().getDay() === today.day
+//     ? (console.log(`Today is ${today.name}`))
+//     : new Date().getDate() === harilain.day
+//     ? (console.log(`TOday is ${harilain.name}`))
+//     : (console.log(`Today is not ${today.name} and not ${harilain.name}`))
+
+// if else ngecek dari atas, switch case langsung
+// case(1) --> case 1: ...., case 2: ...., case 3: ....
+// langsung case 1, tanpa melalui case lain
+
+// Part 3
+const getday = () => {
+    switch (new Date().getDay()) {
+        case 0:
+            return "Sunday";
+            break
+        case 1:
+            return "Monday";
+            break
+        case 2:
+            return "Tuesday";
+            break
+        case 3:
+            return "Wednesday";
+            break
+        case 4:
+            return "Thursday";
+            break
+        case 5:
+            return "Friday";
+            break
+        case 6:
+            return "Saturday";
+            break
+        default:
+            return "Tidak ada pilihan";
+    }
+}
+
+console.log(`Today is ${getday()}`)
